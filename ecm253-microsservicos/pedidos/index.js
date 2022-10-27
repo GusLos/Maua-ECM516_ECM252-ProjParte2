08b483a3e22ef22f0a3e240a1894f64ec3c3cc4e
GUSTAVO LOURENCO LOSADA <19.00345-5@maua.br>
2022-10-27 16:23:42 -0300

    Trocando forma de como os pedidos são enviados e armazenados.
    Mudança por causa de definir preço no mss de conta, resolver problema
    com status (varios status mas não está usando todos).

--- ecm253-microsservicos/pedidos/index.js
+++ ecm253-microsservicos/pedidos/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
-const tipo = require('../../models/tipos-pedido.json');
+// const tipo = require('../../models/tipos-pedido.json');
 const status = require('../../models/tipos-status.json');
 app.use(express.json());
 
@@ -32,26 +32,6 @@ const funcoes = {
     }
 }
 
-function definirPedido(req){
-    tipoPedido = req.body.tipoPedido;
-    // console.log(req);
-    if(tipoPedido === tipo.PF){
-        const { prato } = req.body;
-        // console.log('Escolheu PF.');
-        return { prato, status: status.ENVIANDO_COZINHA };
-    }
-    if (tipoPedido === tipo.MONTAGEM){
-        const { prato, acompanhamentos  } = req.body;
-        // console.log('Escolheu Montagem.');
-        return { prato, acompanhamentos, status: status.ENVIANDO_COZINHA };
-    }
-    if (tipoPedido === tipo.BEBIDA){
-        const { bebida } = req.body;
-        // console.log('Escolheu Bebida.');
-        return { bebida, status: status.ENVIANDO_COPA };
-    }
-}
-
 app.get('/mesas/:idMesa/pedidos', (req, res) => {
     const pedidosPorMesa = BDpedidos.filter((pedido) => pedido.idMesa === req.params.idMesa) || [];
     res.send(pedidosPorMesa);
@@ -63,10 +43,9 @@ app.get('/pedidos', (req, res) => {
 
 app.post('/mesas/:idMesa/pedidos', async (req, res) => {
     const idPedido = uuidv4();
-    const pedido = definirPedido(req);
     const date = new Date();
     const horaPedido = date.toLocaleTimeString();
-    const pedidoNovo = {idPedido, idMesa: req.params.idMesa, tipoPedido: req.body.tipoPedido, horaPedido, ...pedido};
+    const pedidoNovo = {idPedido, idMesa: req.params.idMesa, tipoPedido: req.body.tipoPedido, horaPedido, ...req.body, status: status.ENVIANDO_COZINHA}; // Mudar isso
     BDpedidos.push(pedidoNovo);
     await axios.post('http://localhost:1000/eventos', {
         tipo: 'PedidoCriado',
